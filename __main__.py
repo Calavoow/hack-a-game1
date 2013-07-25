7a0d4e3e8c72b6cee865885c19eac8debc8bd828
calavoow <calavoow@server.fake>
2013-07-25 20:54:34 +0200

    Started on bounce angle adjustment
    Changed position x,y to NumPy array.

--- __main__.py
+++ __main__.py
@@ -28,7 +28,7 @@ class Block(pygame.sprite.Sprite):
 		return array([-1,0])
 
 class Unit(pygame.sprite.Sprite):
-	def __init__(self, surface, x, y, movement):
+	def __init__(self, surface, pos, movement):
 		super(Unit, self).__init__()	
 
 		# Set the surface
@@ -37,103 +37,122 @@ class Unit(pygame.sprite.Sprite):
 		# Collision box
 		self.rect = self.image.get_rect()
 		#Set position
-		self.x = x 
-		self.y = y 
-
+		self.pos = pos 
+		# And movement
 		self.movement = movement 
 	
 	@property
-	def x(self):
-		return self._x
+	def pos(self):
+		return self._pos
 
-	@x.setter
-	def x(self, value):
-		self._x = value
-		self.rect.x = self._x
-
-	@property
-	def center_x( self ):
-		return self.x + self.rect.width/2
+	@pos.setter
+	def pos(self, value):
+		self._pos = value
+		self.rect.x = self._pos[0]
+		self.rect.y = self._pos[1]
 
 	@property
-	def y(self):
-		return self._y
-
-	@y.setter
-	def y(self, value):
-		self._y = value
-		self.rect.y = self._y
-
-	@property
-	def center_y( self ):
-		return self.y + self.rect.width/2
+	def center_pos( self ):
+		return self.pos + array([self.rect.width/2, self.rect.height/2])
 	
 	def update(self):
 		# Collision with lines 
 		intersecting_line = self.intersects_line()
 		if intersecting_line:
+			print "Intersects line at %s" % self.pos
 			#http://stackoverflow.com/questions/573084/how-to-calculate-bounce-angle
 			normal = intersecting_line.get_normal()
 			u = dot( self.movement, normal ) * normal 
 			w = self.movement - u
 			self.movement = w - u
-
-		self.x += self.movement[0]
-		self.y += self.movement[1]
+		
+		# Can't do +=
+		self.pos = self.pos + self.movement
 
 	def intersects_line( self ):
-		movement_line = objects.Line( array([self.center_x, self.center_y]),
-			array([self.center_x + self.movement[0],
-			self.center_y + self.movement[1]]) )
+		movement_line = objects.Line( self.center_pos,
+			self.center_pos + self.movement )
+
+		closest_line = None
 		for line in lines_list:
+			# Get all intersecting lines, with movement_line
+			lines = []
 			if line.intersects( movement_line ):
-				return line
-		return None
+				lines.append( line )
+			smallest_dist = float("inf")
+			for line in lines:
+				intersection_point = line.intersection_point( movement_line )
+				dist = linalg.norm( intersection_point - self.pos )
+				if dist < smallest_dist:
+					smallest_dist = dist
+					closest_line = line
+		
+		return closest_line 
 
 class Player(Unit):
-	def __init__(self, x, y, movement):
-		super(Player, self).__init__(pygame.Surface((16, 16)), x, y, movement)
+	def __init__(self, pos, movement):
+		super(Player, self).__init__(pygame.Surface((16, 16)), pos, movement)
 
 		#The image will be a 16x16 circle 
 		self.image.set_colorkey(( 0, 0, 0 ))		  
 		pygame.draw.circle(self.image, (255,0,0), [8,8], 8)
 		pygame.draw.circle(self.image, (0,0,255), [8,8], 1)
 
-		self.bounce_angle = 0
+		self.bounce_angle = 0.0
 		self.bounce_angles = Queue()
 
 	def update(self):
-		super(Player, self).update()
-
 		# Collision with guard
 		collision_sprite = pygame.sprite.spritecollideany( self, guard_list )
 		if collision_sprite:
 			print "Collided with guard"
 
+		# Collision with target
 		collision_sprite = pygame.sprite.spritecollideany( self, target_list )
 		if collision_sprite:
 			print "Collided with target"
-	
+
+		# Collisiion with lines
+		intersecting_line = self.intersects_line()
+		if intersecting_line:
+			print "Intersects line at %s" % self.pos
+			#http://stackoverflow.com/questions/573084/how-to-calculate-bounce-angle
+			normal = intersecting_line.get_normal()
+			u = dot( self.movement, normal ) * normal 
+			w = self.movement - u
+			self.movement = w - u
+			if not self.bounce_angles.empty():
+				angle = self.bounce_angles.get()
+				rot_matrix = array([[math.cos(angle), math.sin(angle)],
+					[-math.sin(angle), math.cos(angle)]]) 
+				self.movement = dot( rot_matrix, self.movement )
+
+		self.pos = self.pos + self.movement
+
 	def increase_bounce_angle( self ):
-		self.bounce_angle += 1
+		self.bounce_angle += 10.0/360.0 * 2.0 * math.pi
+		print "Increased bounce angle to %s" % self.bounce_angle
 	
 	def decrease_bounce_angle( self ):
-		self.bounce_angle -= 1
+		self.bounce_angle -= 10.0/360.0 * 2.0 * math.pi
+		print "Decreased bounce angle to %s" % self.bounce_angle
 	
 	def confirm_bounce_angle( self ):
+		print "Bounce angle set to %s" % self.bounce_angle
 		self.bounce_angles.put( self.bounce_angle )
+		self.bounce_angle = 0.0
 
 class Guard(Unit):
-	def __init__(self, x, y, movement):
-		super(Guard, self).__init__(pygame.Surface((16, 16)), x, y, movement) 
+	def __init__(self, pos, movement):
+		super(Guard, self).__init__(pygame.Surface((16, 16)), pos, movement) 
 
 		#The image will be a 16x16 circle 
 		self.image.set_colorkey(( 0, 0, 0 ))		  
 		pygame.draw.circle(self.image, (255,255,0), [8,8], 8)
 
 class Target(Unit):
-	def __init__(self, x, y, movement):
-		super(Target, self).__init__(pygame.Surface((16, 16)), x, y, movement) 
+	def __init__(self, pos, movement):
+		super(Target, self).__init__(pygame.Surface((16, 16)), pos, movement) 
 
 		#The image will be a 16x16 circle 
 		self.image.set_colorkey(( 0, 0, 0 ))		  
@@ -158,18 +177,18 @@ objects.Line(array([200, 400]), array([500,400]))]
 
 #Add guards
 guard_list = pygame.sprite.Group()
-# guard_list.add( Guard( 5*screen_width/8, screen_height/2, array([ 0.25, 1 ])))
+guard_list.add( Guard( array([ 5*screen_width/8, screen_height/2 ]), array([ 0.25, 1.0 ])))
 
 all_sprites_list.add( guard_list )
 
 # Add Target
 target_list = pygame.sprite.Group()
-target_list.add( Target( 3*screen_width/8, screen_height/2, array([ 0, 0 ])))
+target_list.add( Target( array([ 3*screen_width/8, screen_height/2 ]), array([ 0.0, 0.0 ])))
 
 all_sprites_list.add( target_list )
 
 #And set the player
-player = Player(screen_width/2, screen_height/2, array([2,1]))
+player = Player( array([ screen_width/2, screen_height/2 ]), array([ 2.0 ,1.0 ]))
 all_sprites_list.add(player)
 
 
@@ -184,15 +203,14 @@ while not done:
 		if event.type == pygame.QUIT: # Exit button presed.
 			done=True # Flag that we are done so we exit this loop
 		elif event.type == pygame.KEYDOWN:
-			if event.key is pygame.K_ESCAPE: # If user clicked close
+			if event.key == pygame.K_ESCAPE: # If user clicked close
 				done=True # Flag that we are done so we exit this loop
-			elif event.key is pygame.K_LEFT:
+			elif event.key == pygame.K_LEFT:
 				player.decrease_bounce_angle()
-			elif event.key is pygame.K_RIGHT:
+			elif event.key == pygame.K_RIGHT:
 				player.increase_bounce_angle()
-			elif event.key is pygame.K_SPACE:
+			elif event.key == pygame.K_SPACE:
 				player.confirm_bounce_angle()
-
 	
 	#Game logic
 	all_sprites_list.update()
